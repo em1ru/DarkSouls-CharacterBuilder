eb5ca4c7e50ca9be7ddc7f99617b3788bbff1b71
GabrielEmile <gabemile7@gmail.com>
2023-11-21 16:32:36 -0300

    Added endurance values

--- script.js
+++ script.js
@@ -113,7 +113,110 @@ document.addEventListener('DOMContentLoaded', () => {
         { vitality: 97, hp: 1888, individualGain: 7 },
         { vitality: 98, hp: 1894, individualGain: 6 },
         { vitality: 99, hp: 1900, individualGain: 6 }
-    ];    
+    ];
+
+    const enduranceStatsTable = [
+        { endurance: 1, stamina: 80, bleedResist: 10, equipLoad: 41 },
+        { endurance: 2, stamina: 80, bleedResist: 10, equipLoad: 42 },
+        { endurance: 3, stamina: 80, bleedResist: 10, equipLoad: 43 },
+        { endurance: 4, stamina: 80, bleedResist: 10, equipLoad: 44 },
+        { endurance: 5, stamina: 80, bleedResist: 10, equipLoad: 45 },
+        { endurance: 6, stamina: 80, bleedResist: 10, equipLoad: 46 },
+        { endurance: 7, stamina: 80, bleedResist: 10, equipLoad: 47 },
+        { endurance: 8, stamina: 88, bleedResist: 33, equipLoad: 48 },
+        { endurance: 9, stamina: 90, bleedResist: 37, equipLoad: 49 },
+        { endurance: 10, stamina: 91, bleedResist: 40, equipLoad: 50 },
+        { endurance: 11, stamina: 93, bleedResist: 44, equipLoad: 51 },
+        { endurance: 12, stamina: 95, bleedResist: 48, equipLoad: 52 },
+        { endurance: 13, stamina: 97, bleedResist: 52, equipLoad: 53 },
+        { endurance: 14, stamina: 98, bleedResist: 56, equipLoad: 54 },
+        { endurance: 15, stamina: 100, bleedResist: 60, equipLoad: 55 },
+        { endurance: 16, stamina: 102, bleedResist: 63, equipLoad: 56 },
+        { endurance: 17, stamina: 104, bleedResist: 65, equipLoad: 57 },
+        { endurance: 18, stamina: 106, bleedResist: 68, equipLoad: 58 },
+        { endurance: 19, stamina: 108, bleedResist: 71, equipLoad: 59 },
+        { endurance: 20, stamina: 110, bleedResist: 73, equipLoad: 60 },
+        { endurance: 21, stamina: 112, bleedResist: 76, equipLoad: 61 },
+        { endurance: 22, stamina: 115, bleedResist: 79, equipLoad: 62 },
+        { endurance: 23, stamina: 117, bleedResist: 81, equipLoad: 63 },
+        { endurance: 24, stamina: 119, bleedResist: 84, equipLoad: 64 },
+        { endurance: 25, stamina: 121, bleedResist: 87, equipLoad: 65 },
+        { endurance: 26, stamina: 124, bleedResist: 89, equipLoad: 66 },
+        { endurance: 27, stamina: 126, bleedResist: 91, equipLoad: 67 },
+        { endurance: 28, stamina: 129, bleedResist: 95, equipLoad: 68 },
+        { endurance: 29, stamina: 131, bleedResist: 97, equipLoad: 69 },
+        { endurance: 30, stamina: 133, bleedResist: 100, equipLoad: 70 },
+        { endurance: 31, stamina: 136, bleedResist: 101, equipLoad: 71 },
+        { endurance: 32, stamina: 139, bleedResist: 101, equipLoad: 72 },
+        { endurance: 33, stamina: 141, bleedResist: 102, equipLoad: 73 },
+        { endurance: 34, stamina: 144, bleedResist: 103, equipLoad: 74 },
+        { endurance: 35, stamina: 146, bleedResist: 104, equipLoad: 75 },
+        { endurance: 36, stamina: 149, bleedResist: 104, equipLoad: 76 },
+        { endurance: 37, stamina: 152, bleedResist: 105, equipLoad: 77 },
+        { endurance: 38, stamina: 154, bleedResist: 106, equipLoad: 78 },
+        { endurance: 39, stamina: 157, bleedResist: 107, equipLoad: 79 },
+        { endurance: 40, stamina: 160, bleedResist: 107, equipLoad: 80 },
+        // Endurance levels beyond 40 have stamina capped at 160
+        { endurance: 41, stamina: 160, bleedResist: 108, equipLoad: 81 },
+        { endurance: 42, stamina: 160, bleedResist: 109, equipLoad: 82 },
+        { endurance: 43, stamina: 160, bleedResist: 109, equipLoad: 83 },
+        { endurance: 44, stamina: 160, bleedResist: 110, equipLoad: 84 },
+        { endurance: 45, stamina: 160, bleedResist: 111, equipLoad: 85 },
+        { endurance: 46, stamina: 160, bleedResist: 112, equipLoad: 86 },
+        { endurance: 47, stamina: 160, bleedResist: 112, equipLoad: 87 },
+        { endurance: 48, stamina: 160, bleedResist: 113, equipLoad: 88 },
+        { endurance: 49, stamina: 160, bleedResist: 114, equipLoad: 89 },
+        { endurance: 50, stamina: 160, bleedResist: 114, equipLoad: 90 },
+        { endurance: 51, stamina: 160, bleedResist: 115, equipLoad: 91 },
+        { endurance: 52, stamina: 160, bleedResist: 116, equipLoad: 92 },
+        { endurance: 53, stamina: 160, bleedResist: 117, equipLoad: 93 },
+        { endurance: 54, stamina: 160, bleedResist: 117, equipLoad: 94 },
+        { endurance: 55, stamina: 160, bleedResist: 118, equipLoad: 95 },
+        { endurance: 56, stamina: 160, bleedResist: 119, equipLoad: 96 },
+        { endurance: 57, stamina: 160, bleedResist: 120, equipLoad: 97 },
+        { endurance: 58, stamina: 160, bleedResist: 120, equipLoad: 98 },
+        { endurance: 59, stamina: 160, bleedResist: 121, equipLoad: 99 },
+        { endurance: 60, stamina: 160, bleedResist: 122, equipLoad: 100 },
+        { endurance: 61, stamina: 160, bleedResist: 122, equipLoad: 101 },
+        { endurance: 62, stamina: 160, bleedResist: 123, equipLoad: 102 },
+        { endurance: 63, stamina: 160, bleedResist: 124, equipLoad: 103 },
+        { endurance: 64, stamina: 160, bleedResist: 125, equipLoad: 104 },
+        { endurance: 65, stamina: 160, bleedResist: 125, equipLoad: 105 },
+        { endurance: 66, stamina: 160, bleedResist: 126, equipLoad: 106 },
+        { endurance: 67, stamina: 160, bleedResist: 127, equipLoad: 107 },
+        { endurance: 68, stamina: 160, bleedResist: 128, equipLoad: 108 },
+        { endurance: 69, stamina: 160, bleedResist: 128, equipLoad: 109 },
+        { endurance: 70, stamina: 160, bleedResist: 129, equipLoad: 110 },
+        { endurance: 71, stamina: 160, bleedResist: 130, equipLoad: 111 },
+        { endurance: 72, stamina: 160, bleedResist: 130, equipLoad: 112 },
+        { endurance: 73, stamina: 160, bleedResist: 131, equipLoad: 113 },
+        { endurance: 74, stamina: 160, bleedResist: 132, equipLoad: 114 },
+        { endurance: 75, stamina: 160, bleedResist: 133, equipLoad: 115 },
+        { endurance: 76, stamina: 160, bleedResist: 133, equipLoad: 116 },
+        { endurance: 77, stamina: 160, bleedResist: 134, equipLoad: 117 },
+        { endurance: 78, stamina: 160, bleedResist: 135, equipLoad: 118 },
+        { endurance: 79, stamina: 160, bleedResist: 136, equipLoad: 119 },
+        { endurance: 80, stamina: 160, bleedResist: 136, equipLoad: 120 },
+        { endurance: 81, stamina: 160, bleedResist: 137, equipLoad: 121 },
+        { endurance: 82, stamina: 160, bleedResist: 138, equipLoad: 122 },
+        { endurance: 83, stamina: 160, bleedResist: 138, equipLoad: 123 },
+        { endurance: 84, stamina: 160, bleedResist: 139, equipLoad: 124 },
+        { endurance: 85, stamina: 160, bleedResist: 140, equipLoad: 125 },
+        { endurance: 86, stamina: 160, bleedResist: 141, equipLoad: 126 },
+        { endurance: 87, stamina: 160, bleedResist: 141, equipLoad: 127 },
+        { endurance: 88, stamina: 160, bleedResist: 142, equipLoad: 128 },
+        { endurance: 89, stamina: 160, bleedResist: 143, equipLoad: 129 },
+        { endurance: 90, stamina: 160, bleedResist: 143, equipLoad: 130 },
+        { endurance: 91, stamina: 160, bleedResist: 144, equipLoad: 131 },
+        { endurance: 92, stamina: 160, bleedResist: 145, equipLoad: 132 },
+        { endurance: 93, stamina: 160, bleedResist: 146, equipLoad: 133 },
+        { endurance: 94, stamina: 160, bleedResist: 146, equipLoad: 134 },
+        { endurance: 95, stamina: 160, bleedResist: 147, equipLoad: 135 },
+        { endurance: 96, stamina: 160, bleedResist: 148, equipLoad: 136 },
+        { endurance: 97, stamina: 160, bleedResist: 149, equipLoad: 137 },
+        { endurance: 98, stamina: 160, bleedResist: 149, equipLoad: 138 },
+        { endurance: 99, stamina: 160, bleedResist: 150, equipLoad: 139 },
+    ];
 
     // Weapons data array with stats
     const weapons = {
@@ -383,12 +486,26 @@ document.addEventListener('DOMContentLoaded', () => {
         leftHand1.appendChild(option);
     });
 
+    function getStaminaFromEndurance(endurance) {
+        if (endurance >= 40) return 160;
+        const row = enduranceStatsTable.find(row => row.endurance === endurance);
+        return row ? row.stamina : null;
+    }
+
     function updateStats() {
         const vitalityValue = parseInt(vitality.value, 10);
+        const enduranceValue = parseInt(endurance.value, 10);
+        
+        // Ensure we are using integers for table lookups
         const hp = getHpFromVitality(vitalityValue);
+        const stamina = getStaminaFromEndurance(enduranceValue);
     
-        if(hp !== null) {
-            document.getElementById('hpValue').textContent = hp.toString();
+        if (hp !== null) {
+            document.getElementById('hpValue').textContent = hp;
+        }
+
+        if (stamina !== null) {
+            document.getElementById('staminaValue').textContent = stamina;
         }
     
         console.log('Stats updated');
@@ -423,11 +540,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     soulLevel.addEventListener('change', updateStats);
     vitality.addEventListener('change', updateStats);
+    endurance.addEventListener('change', updateStats);
 
     updateStatsForClass(classes.find(cls => cls.name.toLowerCase() === classSelect.value));
 
-    document.getElementById('calculate').addEventListener('click', () => {
-        updateStats();
-    });
+    document.getElementById('calculate').addEventListener('click', updateStats);
 
 });
